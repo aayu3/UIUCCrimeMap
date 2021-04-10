6ab67d1ca4154edbf4698ed8cd0cb890516b08d8
Aaron Yu <67659126+aayu3@users.noreply.github.com>
2021-04-10 13:30:06 -0700

    Added api dependecies to api folder.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./api/routes");
 const cors = require("cors");
 const path = require('path');
 
