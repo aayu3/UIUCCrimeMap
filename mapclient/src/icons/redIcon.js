9fb064bff0d9bf22198ec1f1e69a1c9d4edf5e66
Aaron Yu <67659126+aayu3@users.noreply.github.com>
2021-04-10 16:24:55 -0700

    Added shadows
    Added shadows to show outlines of the icons for the map.

--- mapclient/src/icons/redIcon.js
+++ mapclient/src/icons/redIcon.js
@@ -1,15 +1,17 @@
 import L from 'leaflet';
 import redMarker from "./redMarker.svg";
+import shadowMarker from "./shadowMarker.svg";
+
 
 const redIcon = new L.Icon({
     iconUrl: redMarker,
     iconRetinaUrl: redMarker,
     iconAnchor: [0,0],
     popupAnchor:  [15.5,10],
-    shadowUrl: null,
-    shadowSize: null,
-    shadowAnchor: null,
-    iconSize: new L.Point(30, 30),
+    shadowUrl: shadowMarker,
+    shadowSize: [36, 36],
+    shadowAnchor: [2.5,2],
+    iconSize: [30, 30],
     className: 'redIcon'
 });
 
