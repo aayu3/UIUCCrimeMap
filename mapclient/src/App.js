800c6b07f1873bb8e01933b052f17dd8f99e698e
Aaron Yu <aaronyu3@illinois.edu>
2021-05-30 00:31:53 -0700

    change dates shown underneath filter buttons.

--- mapclient/src/App.js
+++ mapclient/src/App.js
@@ -258,11 +258,11 @@ const Range = createSliderWithTooltip(Slider.Range);
               <h2><b>Legend:</b></h2>
               <h5>Crimes within...</h5>
               <button onClick={this.changeToRed} type="button" class="btn btn-danger">Filter Red Only</button> <br></br>
-              {this.state.thresholds[0]} Days.  
+              {this.state.thresholds[1]} Days.  
               <br></br>
               <br></br>
               <button onClick={this.changeToYellow} type="button" class="btn btn-warning">Filter Yellow Only</button> <br></br>
-              {this.state.thresholds[1]} Days.
+              {this.state.thresholds[2]} Days.
               <br></br>
               <br></br>
               <button onClick={this.resetMap} type="button" class="btn btn-primary">Reset Map</button> 
