39b6942cf958e866390517783ef9e895adfbb5c6
Aaron Yu <aaronyu3@illinois.edu>
2021-07-23 02:04:48 -0700

    updated the teams page

--- mapclient/src/components/Team/Team.js
+++ mapclient/src/components/Team/Team.js
@@ -11,6 +11,8 @@ import illiaborzov from './images/illiapfp.jpg';
 const team = () => {
     return (
         <div className="team">
+            <h2>Meet the Team</h2>
+            <div className="break"></div>
             <div>
                 <a href="https://github.com/aayu3"><img className="profilepic" src={aaronyu} alt="Aaron Yu"/></a>           
                 <div className="name">
@@ -20,15 +22,7 @@ const team = () => {
                     Aaron is a Freshman at UIUC in Mathematics + Computer Science. In his spare time he enjoys playing with his cat and reading.
                 </div>
             </div>
-            <div>
-                <a href="https://github.com/r0ckwav3"><img className="profilepic" src={petervandervelde} alt="Peter Vandervelde"/></a>
-                <div className="name">
-                    Peter Vandervelde
-                </div>
-                <div className="bio">
-                    Peter is a High Schooler at Proof School who is interested in CS and Mathematics. His hobbies include acrobatics and digital drawing. 
-                </div>
-            </div>
+            
             <div>
                 <a href="https://github.com/jchoi25"><img className="profilepic" src={profilepic} alt="Juyoung Choi"/></a>
                 <div className="name">
@@ -36,17 +30,6 @@ const team = () => {
                 </div>
                 <div className="bio">
                     Juyoung is a Freshman at UIUC majoring in Physics from South Korea. He enjoys observing the night sky and playing the guitar.
-                </div>
-            </div>
-            <div className="break"></div>
-            <div>
-                <a href="https://github.com/Angel-Shah"><img className="profilepic" src={angelshah} alt="Angel Shah"/></a>
-                
-                <div className="name">
-                    Angel Shah
-                </div>
-                <div className="bio">
-                    Angel is a sophomore studying Computer Engineering at the University of Waterloo. In his spare time, he loves learning piano and reading. <i class="fa fa-book"></i>
                 </div>
             </div>
             <div>
@@ -59,15 +42,30 @@ const team = () => {
                     Illia is a freshman studying Computer Science at NYU. In addition to CS, he enjoys linguistics and graphic design.
                 </div>
             </div>
+            <div className="break"></div>
+           
+            <h2>Previous Contributors</h2>
+            <div className="break"></div>
             <div>
-                <a href="https://github.com/mraahemi"><img className="profilepic" src={mohammad} alt="Mohammad"/></a>
+                <a href="https://github.com/r0ckwav3"><img className="profilepic" src={petervandervelde} alt="Peter Vandervelde"/></a>
                 <div className="name">
-                    Mohammad Raahemi
+                    Peter Vandervelde
                 </div>
                 <div className="bio">
-                    Mohammad is an MSc student at the University of Ottawa.<br></br> He is studying CS as his major program, and his minor stream is in Data Science.
+                    Peter is a High Schooler at Proof School who is interested in CS and Mathematics. His hobbies include acrobatics and digital drawing. 
                 </div>
             </div>
+            <div>
+                <a href="https://github.com/Angel-Shah"><img className="profilepic" src={angelshah} alt="Angel Shah"/></a>
+                
+                <div className="name">
+                    Angel Shah
+                </div>
+                <div className="bio">
+                    Angel is a sophomore studying Computer Engineering at the University of Waterloo. In his spare time, he loves learning piano and reading. <i class="fa fa-book"></i>
+                </div>
+            </div>
+        
         </div>
     )
 }
