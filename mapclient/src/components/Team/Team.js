2b4dddbf318d227be71da60dd400dd796c5ca368
jchoi25 <juyoung7@illinois.edu>
2021-08-03 23:15:25 +0900

    design change: new design for team page

--- mapclient/src/components/Team/Team.js
+++ mapclient/src/components/Team/Team.js
@@ -9,71 +9,102 @@ import illiaborzov from "./images/illiapfp.jpg";
 
 const team = () => {
   return (
-    <div className="team" style={{overflowY:"auto"}}>
+    <div className="team-page" style={{overflowY:"auto"}}>
       <h2>Meet the Team</h2>
-      <div className="break"></div>
-      <div>
-        <a href="https://github.com/aayu3">
-          <img className="profilepic" src={aaronyu} alt="Aaron Yu" />
-        </a>
-        <div className="name">Aaron Yu</div>
-        <div className="bio">
-          Aaron is a Freshman at UIUC in Mathematics + Computer Science. In his
-          spare time he enjoys playing with his cat and reading.
+      <section className="team">
+        <div className="item">
+          <div className="box">
+            <div className="profile-pic">
+              <a href="https://github.com/aayu3">
+                <img src={aaronyu} alt="Aaron Yu" />
+              </a>
+            </div>
+            <div className="bio">
+              <h4>Aaron Yu</h4>
+              <p>
+                Aaron is a Freshman at UIUC in Mathematics + Computer Science. In his
+                spare time he enjoys playing with his cat and reading.
+              </p>
+            </div> 
+          </div>
         </div>
-      </div>
 
-      <div>
-        <a href="https://github.com/jchoi25">
-          <img className="profilepic" src={juyoungchoi} alt="Juyoung Choi" />
-        </a>
-        <div className="name">Juyoung Choi</div>
-        <div className="bio">
-          Juyoung is a Freshman at UIUC majoring in Physics from South Korea. He
-          enjoys observing the night sky and playing the guitar.
+        <div className="item">
+          <div className="box">
+            <div className="profile-pic">
+              <a href="https://github.com/jchoi25">
+                <img src={juyoungchoi} alt="Juyoung Choi" />
+              </a>
+            </div>
+            <div className="bio">
+              <h4>Juyoung Choi</h4>
+              <p>
+                Juyoung is a Freshman at UIUC majoring in Physics from South Korea. He
+                enjoys observing the night sky and playing the guitar.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div>
-        <a href="https://github.com/zer0key123">
-          <img className="profilepic" src={illiaborzov} alt="Illia Borzov" />
-        </a>
 
-        <div className="name">Illia Borzov</div>
-        <div className="bio">
-          Illia is a freshman studying Computer Science at NYU. In addition to
-          CS, he enjoys linguistics and graphic design.
+        <div className="item">
+          <div className="box">
+            <div className="profile-pic">
+              <a href="https://github.com/zer0key123">
+                <img src={illiaborzov} alt="Illia Borzov" />
+              </a>
+            </div>
+            <div className="bio">
+            <h4>Illia Borzov</h4>
+              <p>
+                Illia is a freshman studying Computer Science at NYU. In addition to
+                CS, he enjoys linguistics and graphic design.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="break"></div>
+      </section>
 
       <h2>Previous Contributors</h2>
-      <div className="break"></div>
-      <div>
-        <a href="https://github.com/r0ckwav3">
-          <img
-            className="profilepic"
-            src={petervandervelde}
-            alt="Peter Vandervelde"
-          />
-        </a>
-        <div className="name">Peter Vandervelde</div>
-        <div className="bio">
-          Peter is a High Schooler at Proof School who is interested in CS and
-          Mathematics. His hobbies include acrobatics and digital drawing.
-        </div>
-      </div>
-      <div>
-        <a href="https://github.com/Angel-Shah">
-          <img className="profilepic" src={angelshah} alt="Angel Shah" />
-        </a>
+      <section className="team">
+        <div className="item">
+         <div className="box">
+          <div className="profile-pic">
+            <a href="https://github.com/r0ckwav3">
+              <img
+                src={petervandervelde}
+                alt="Peter Vandervelde"
+              />
+            </a>
+          </div>
 
-        <div className="name">Angel Shah</div>
-        <div className="bio">
-          Angel is a sophomore studying Computer Engineering at the University
-          of Waterloo. In his spare time, he loves learning piano and reading.{" "}
-          <i class="fa fa-book"></i>
+          <div className="bio">
+            <h4>Peter Vandervelde</h4>
+            <p>
+              Peter is a High Schooler at Proof School who is interested in CS and
+              Mathematics. His hobbies include acrobatics and digital drawing.
+            </p>
+          </div>
         </div>
       </div>
+
+        <div className="item">
+          <div className="box">
+            <div className="profile-pic">
+              <a href="https://github.com/Angel-Shah">
+                <img src={angelshah} alt="Angel Shah" />
+              </a>
+            </div>
+            <div className="bio">
+              <h4>Angel Shah</h4>
+              <p>
+                Angel is a sophomore studying Computer Engineering at the University
+                of Waterloo. In his spare time, he loves learning piano and reading.{" "}
+                <i class="fa fa-book"></i>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
